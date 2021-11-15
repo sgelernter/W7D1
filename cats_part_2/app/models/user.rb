c50300513e006362370349a0357db7778f301cc9
jdtavarez <juliodtavarez@gmail.com>
2021-11-15 14:48:51 -0500

    create users controller and routes for actions new and create; add new view to users

--- cats_part_2/app/models/user.rb
+++ cats_part_2/app/models/user.rb
@@ -2,7 +2,7 @@ class User < ApplicationRecord
 
     validates :username, :session_token, presence: true, uniqueness: true 
     validates :password_digest, presence: true 
-    validates :password, presence: true, lengh: {minimum: 6}, allow_nil: true 
+    validates :password, presence: true, length: {minimum: 6}, allow_nil: true 
 
     after_initialize :ensure_session_token
     attr_reader :password
